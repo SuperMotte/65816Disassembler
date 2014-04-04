5818f3245a0cac32a479d8d83b47ff57ddbda5cf
Tobias Loose <TobiasLoose@gmx.de>
2014-04-04 21:16:27 +0200

    Remove myself from explicit copyright headers

--- snesdisasm/Instructions.cpp
+++ snesdisasm/Instructions.cpp
@@ -1,7 +1,4 @@
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2014  Tobias Loose <email>
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
